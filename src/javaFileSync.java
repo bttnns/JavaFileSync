c7ec422b2cfcb1170a225e93ca3ad1b9361e8562
quaa <quaa55@gmail.com>
2010-11-20 19:26:40 -0600

    Created new class for server

--- src/javaFileSync.java
+++ src/javaFileSync.java
@@ -30,39 +30,8 @@ public class javaFileSync {
 	}
 
 	public static void server() throws Exception {
-		System.out.println("Starting File Sync Server!");
-		
-		ServerSocket servsock = new ServerSocket(PORT_NUMBER);
-		
-		while (true) {
-			Socket sock = servsock.accept();
-
-			ObjectInputStream ois = new ObjectInputStream(sock.getInputStream());
-			String baseDir = (String) ois.readObject();
-			
-			File fBaseDir = new File(baseDir);
-			
-			if(fBaseDir.exists() && fBaseDir.isDirectory()){
-				System.out.println(baseDir + " is a directory and exists!");
-/*				System.out.print("Sending file '" + fileName + "' to: " + sock.getInetAddress() + "...");
-				ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
-				oos.writeObject("1");
-				byte[] mybytearray = new byte[(int) f.length()];
-				BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
-				bis.read(mybytearray, 0, mybytearray.length);
-				OutputStream os = sock.getOutputStream();
-				os.write(mybytearray, 0, mybytearray.length);
-				os.flush();
-				System.out.println(" DONE!");
-*/			}else{
-				System.out.println(baseDir + " Exists?: " + fBaseDir.exists() + " IsDirectory?: " + fBaseDir.isDirectory());
-/*				System.out.println(sock.getInetAddress() + " requested " + fileName + ", but it does not exist!");
-				ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
-				oos.writeObject("0");
-				oos.flush();
-*/			}
-			sock.close();
-		}
+		Server s = new Server(PORT_NUMBER);
+		s.startServer();
 	}
 	
 	public static void client(String dirName, String serverIP) throws Exception {
