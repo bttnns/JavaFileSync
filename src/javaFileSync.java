8f9c485e57eca17560d0d62ee700f15e4969c255
quaa <quaa55@gmail.com>
2010-11-20 18:02:34 -0600

    Simple sockets to check if a base directory exists

--- src/javaFileSync.java
+++ src/javaFileSync.java
@@ -1,38 +1,73 @@
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 
 public class javaFileSync {
 
 	public static void main(String[] args) {
-		if(args.length > 0) {
-			if (args[0].equalsIgnoreCase("-s")) {
-				server();
-			} else if (args[0].equalsIgnoreCase("-c") && args.length > 1 && args.length < 3) {
-				client(args[2], args[1]);
-			} else if (args[0].equalsIgnoreCase("-t") && args.length > 1 && args.length < 4) {
-				testing(args[2], args[1]);
+		try {
+			if(args.length > 0) {
+				if (args[0].equalsIgnoreCase("-s")) {
+					server();
+				} else if (args[0].equalsIgnoreCase("-c") && args.length > 1 && args.length < 3) {
+					client(args[2], args[1]);
+				} else {
+					System.out.println("Invalid entry. Useage: java javaFileSync [-s] [-c [server IP] [dir to sync]]");
+				}
 			} else {
 				System.out.println("Invalid entry. Useage: java javaFileSync [-s] [-c [server IP] [dir to sync]]");
 			}
-		} else {
-			System.out.println("Invalid entry. Useage: java javaFileSync [-s] [-c [server IP] [dir to sync]]");
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
 		}
 	}
 
-	public static void server(){
-		System.out.println("Server selected!");
+	public static void server() throws Exception {
+		System.out.println("Starting File Sync Server!");
+		
+		ServerSocket servsock = new ServerSocket(17555);
+		
+		while (true) {
+			Socket sock = servsock.accept();
+
+			ObjectInputStream ois = new ObjectInputStream(sock.getInputStream());
+			String baseDir = (String) ois.readObject();
+			
+			File fBaseDir = new File(baseDir);
+			
+			if(fBaseDir.exists() && fBaseDir.isDirectory()){
+				System.out.println(baseDir + " is a directory and exists!");
+/*				System.out.print("Sending file '" + fileName + "' to: " + sock.getInetAddress() + "...");
+				ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
+				oos.writeObject("1");
+				byte[] mybytearray = new byte[(int) f.length()];
+				BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
+				bis.read(mybytearray, 0, mybytearray.length);
+				OutputStream os = sock.getOutputStream();
+				os.write(mybytearray, 0, mybytearray.length);
+				os.flush();
+				System.out.println(" DONE!");
+*/			}else{
+				System.out.println(baseDir + " Exists?: " + fBaseDir.exists() + " IsDirectory?: " + fBaseDir.isDirectory());
+/*				System.out.println(sock.getInetAddress() + " requested " + fileName + ", but it does not exist!");
+				ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
+				oos.writeObject("0");
+				oos.flush();
+*/			}
+			sock.close();
+		}
 	}
 	
-	public static void client(String dirName, String serverIP){
+	public static void client(String dirName, String serverIP) {
 		System.out.println("Client Selected!");
 		System.out.println("Dir to sync: " + dirName);
 		System.out.println("Server IP: " + serverIP);
-	}
-	
-	public static void testing(String dirName, String serverIP){
-		System.out.println("Secret Testing function!");
-		File f = new File(dirName);
-		visitAllFiles(f);
 	}
 	
 	// Process all files and directories under dir
